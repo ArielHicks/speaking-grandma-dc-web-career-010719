575b0b5038f8835de6a1e2235d1808aca4bcb9ab
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-22 17:12:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
 
-if phrase.downcase
+if phrase == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-else phrase.upcase
+else phrase == phrase.upcase
   puts "I LOVE YOU TOO PUMPKIN!"
 
 # If you shout, she can hear you (or at least she thinks so)
